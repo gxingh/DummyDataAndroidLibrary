d9737b4ceb68501eb081eb3bea73cacf883633d2
Gagandeep Singh <gagandeep.singh@Kony.com>
2018-11-27 17:24:53 +0530

    DummyData returns list of string

--- app/src/main/java/com/example/kh2315/recyclerdummydata/MainActivity.java
+++ app/src/main/java/com/example/kh2315/recyclerdummydata/MainActivity.java
@@ -2,12 +2,65 @@ package com.example.kh2315.recyclerdummydata;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.recyclerdummydatalibrary.DummyData;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    RecyclerView recycler;
+    List<String> list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        list = new ArrayList<>();
+        list = DummyData.getListString(10);
+
+        recycler = findViewById(R.id.recycler);
+        RecyclerAdapter adapter = new RecyclerAdapter(list);
+        recycler.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        recycler.setAdapter(adapter);
+
+    }
+
+    private class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyViewHolder>{
+        List<String> list;
+        public RecyclerAdapter(List<String> list) {
+            this.list = list;
+        }
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(getApplicationContext()).inflate(R.layout.recycler, parent, false);
+            return new MyViewHolder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            holder.title.setText(list.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return list.size();
+        }
+
+        public class MyViewHolder extends RecyclerView.ViewHolder {
+            TextView title;
+            public MyViewHolder(View itemView) {
+                super(itemView);
+                title = itemView.findViewById(R.id.title);
+            }
+        }
     }
 }
--- recyclerdummydatalibrary/src/main/java/com/example/recyclerdummydatalibrary/DummyData.java
+++ recyclerdummydatalibrary/src/main/java/com/example/recyclerdummydatalibrary/DummyData.java
@@ -1,7 +1,18 @@
 package com.example.recyclerdummydatalibrary;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DummyData {
-    public static String getStringData(){
-        return "Something";
+    static String lorem = "Lorem Ipsum is simply dummy text";
+
+    public static List<String> getListString(int listSize){
+        List<String> list = new ArrayList<>();
+
+        for(int i=0; i<listSize; i++){
+            list.add(lorem);
+        }
+
+        return list;
     }
 }
