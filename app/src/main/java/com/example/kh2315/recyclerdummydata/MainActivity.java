0eed5947da414bcf32e7a6a12ee1f7115977dbda
Gagandeep Singh <gagandeep.singh@Kony.com>
2018-11-27 16:02:40 +0530

    initial commit

---
+++ app/src/main/java/com/example/kh2315/recyclerdummydata/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.kh2315.recyclerdummydata;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ recyclerdummydatalibrary/src/main/java/com/example/recyclerdummydatalibrary/DummyData.java
@@ -0,0 +1,7 @@
+package com.example.recyclerdummydatalibrary;
+
+public class DummyData {
+    public static String getStringData(){
+        return "Something";
+    }
+}
